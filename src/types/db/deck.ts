1183b0732b696f3a506d3e630a5331cb4ba3be4c
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-08-30 17:44:57 +0100

    Add types to represent database rows

--- src/types/db/deck.ts
+++ src/types/db/deck.ts
@@ -1,3 +1,5 @@
+import { CardWithStreak } from "./card";
+
 export interface DeckCandidate {
     name: string;
 }
@@ -6,3 +8,8 @@ export interface Deck extends DeckCandidate {
     id: number;
     created_at: Date;
 }
+
+export interface DeckContent extends Deck {
+    cards: CardWithStreak[];
+}
+export { CardWithStreak };
